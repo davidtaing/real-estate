cbfa73cae17909d2a87033973248dbd0e97eb23f
davidtaing <adavidtaing@gmail.com>
2021-09-13 01:33:05 +1000

    init(firebase): configs

--- users-api/src/config/config.js
+++ users-api/src/config/config.js
@@ -3,6 +3,15 @@ dotenv.config();
 
 const config = {
   PORT: process.env.PORT,
+  FIREBASE_CONFIGS: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
+  }
 };
 
 export default config;
