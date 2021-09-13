039301f1c6f24fd63a8e7a1389e177fcdbccd9f8
davidtaing <adavidtaing@gmail.com>
2021-09-14 00:26:23 +1000

    tests: refactor tests into different describes

--- users-api/tests/register.controller.test.js
+++ users-api/tests/register.controller.test.js
@@ -35,51 +35,55 @@ describe("Testing User Registration", () => {
     }
   });
 
-  it("Register User: successfully register new user and respond with 204 status code.", async () => {
-    chai.request(server)
-      .post("/register")
-      .send(DEFAULT_TEST_USER)
-      .end((err, res) => {
-        expect(res).to.have.status(204);
-      });
+  describe("Successful User Registration", () => {
+    it("Register User: successfully register new user and respond with 204 status code.", async () => {
+      chai.request(server)
+        .post("/register")
+        .send(DEFAULT_TEST_USER)
+        .end((err, res) => {
+          expect(res).to.have.status(204);
+        });
+    });
   });
 
-  it("Register Same User Again: handle auth/email-already-in-use error appropiately and respond with 204 status.", async () => {
-    chai.request(server)
-      .post("/register")
-      .send(DEFAULT_TEST_USER)
-      .end((err, res) => {
-         expect(res).to.have.status(204);
-      });
-  });
-
-  it("Invalid Email Input: respond with 400 status.", () => {
-    let user = {
-      // Create invalid email string
-      email: '@' + DEFAULT_TEST_USER.email,
-      password: DEFAULT_TEST_USER.password,
-    };
-
-    chai.request(server)
-      .post("/register")
-      .send(user)
-      .end((err, res) => {
-        expect(res).to.have.status(400);
-     });
-  });
-
-  it("Empty User Object: respond with 400 status.", () => {
-    // Invalid user object
-    let user = {
-      email: "",
-      password: "",
-    };
-
-    chai.request(server)
-      .post("/register")
-      .send(user)
-      .end((err, res) => {
-        expect(res).to.have.status(400);
-     });
+  describe("Failed User Registration", () => {
+    it("Register Same User Again: handle auth/email-already-in-use error appropiately and respond with 204 status.", async () => {
+      chai.request(server)
+        .post("/register")
+        .send(DEFAULT_TEST_USER)
+        .end((err, res) => {
+           expect(res).to.have.status(204);
+        });
+    });
+  
+    it("Invalid Email Input: respond with 400 status.", () => {
+      let user = {
+        // Create invalid email string
+        email: '@' + DEFAULT_TEST_USER.email,
+        password: DEFAULT_TEST_USER.password,
+      };
+  
+      chai.request(server)
+        .post("/register")
+        .send(user)
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+       });
+    });
+  
+    it("Empty User Object: respond with 400 status.", () => {
+      // Invalid user object
+      let user = {
+        email: "",
+        password: "",
+      };
+  
+      chai.request(server)
+        .post("/register")
+        .send(user)
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+       });
+    });
   });
 })
