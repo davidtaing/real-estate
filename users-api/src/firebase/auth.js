f6e9d2f22c5f6853feaa3e9ad0fb5ac252a815f6
davidtaing <adavidtaing@gmail.com>
2021-09-13 17:58:15 +1000

    init(firebase): init auth emulator settings

--- users-api/src/firebase/auth.js
+++ users-api/src/firebase/auth.js
@@ -1,5 +1,8 @@
-import { getAuth } from "firebase/auth";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
 
 const auth = getAuth();
+if (process.env.NODE_ENV === "development") {
+  connectAuthEmulator(auth, "http://localhost:9099");
+}
 
 export default auth;
