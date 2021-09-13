01a6b3d79a5226cc859414c8f36c4643e031dff4
davidtaing <adavidtaing@gmail.com>
2021-09-13 20:23:44 +1000

    init(tests): add login.controller.test file
    No implementation yet because I need to implement the Register route first.

---
+++ users-api/tests/login.controller.test.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+
+/**
+ * TODO: Test Flow
+ * Source: https://firebase.google.com/docs/emulator-suite/connect_auth
+ * 
+ * 1. Create Users with Authentication signUp REST Endpoint
+ * 2. Sign in users using the emails and passwords to perform tests.
+ * 3. If applicable to your tests, fetch available out-of-band email verification codes from the emulator-specific REST endpont.
+ * 4. Flush user records with the emulator-specific REST endpoint for clearing data.
+ */
