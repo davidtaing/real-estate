c513862e80ce0d5bbc93e0bd0230a2615f94e4fa
davidtaing <adavidtaing@gmail.com>
2021-09-14 00:14:14 +1000

    test: init test skeleton

--- users-api/tests/login.controller.test.js
+++ users-api/tests/login.controller.test.js
@@ -1,11 +1,39 @@
-import { expect } from 'chai';
+import chai from "chai";
+import chaiHttp from "chai-http";
+import fetch from "cross-fetch";
+
+import config from "../src/config/config";
+import server from "../src/app";
+
+import { DEFAULT_TEST_USER, WIPE_USERS_URL } from "./config";
+
+chai.use(chaiHttp);
+const { expect } = chai;
 
 /**
- * TODO: Test Flow
- * Source: https://firebase.google.com/docs/emulator-suite/connect_auth
- * 
- * 1. Create Users with Authentication signUp REST Endpoint
- * 2. Sign in users using the emails and passwords to perform tests.
- * 3. If applicable to your tests, fetch available out-of-band email verification codes from the emulator-specific REST endpont.
- * 4. Flush user records with the emulator-specific REST endpoint for clearing data.
+ * Before: 
+ *     Wipe users database, then create user using default user in config 
+ * Tests:
+ *     - Successful Login (Valid Credentials)
+ *     - Invalid Password
+ *     - User Doesn't Exist
+ *     - Invalid Body (Empty Object)
+ *     - Invalid Body (Missing Email)
+ * After:
+ *     Wipe users database
  */
+describe("Testing User Login", () => {
+  // before
+
+  // login success: return 200 status and auth tokens
+
+  // invalid password: return 401 status
+
+  // user doesn't exist: return 401 status
+
+  // invalid body (empty object): return 400 status
+
+  // invalid body (missing email): return 400 status
+
+  // after
+});
