036398d466f8438aadaef3c7956d7f3db1468e2c
davidtaing <adavidtaing@gmail.com>
2021-09-13 19:58:07 +1000

    feat(login): add error handling and refactor jwt

--- users-api/src/controllers/login.controller.js
+++ users-api/src/controllers/login.controller.js
@@ -11,12 +11,27 @@ const loginController = async (req, res, next) => {
     // TODO Sanitize and Validate Input
 
     // Call Firebase
-    let jwt = await signInWithEmailAndPassword(auth, email, password);
-    
+    const { user } = (await signInWithEmailAndPassword(auth, email, password));
+    const jwt = user.stsTokenManager;
+
     // Return HTTP 200 & JWT Token if Successful
     res.status(200).json(jwt);
   } catch (err) {
-    next(err);
+    console.log(err.code);
+
+    switch (err.code) {
+      case "auth/user-not-found":
+      case "auth/wrong-password":
+      case "auth/user-not-found":
+        return res.status(401).json({ status: 401, message: "Unauthorized"});
+      case "auth/missing-email":
+      case "auth/missing-password":
+      case "auth/invalid-password":
+      case "auth/invalid-email":
+        return res.status(400).json({ status: 400, message: "Bad Request"});
+      default:
+        return next(err);
+    }
   }
 };
 
