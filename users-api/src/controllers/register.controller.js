1f481b6e3b9a92bce779ca3165831a718c0579cb
davidtaing <adavidtaing@gmail.com>
2021-09-13 23:49:12 +1000

    fix: handle empty object input

--- users-api/src/controllers/register.controller.js
+++ users-api/src/controllers/register.controller.js
@@ -9,6 +9,9 @@ const registerController = async (req, res, next) => {
     let { email, password } = req.body;
 
     // TODO Sanitize and Validate Input
+    if (!email || !password) {
+      throw Error("400");
+    }
 
     // Call Firebase
     const user = await createUserWithEmailAndPassword(auth, email, password);
@@ -19,6 +22,10 @@ const registerController = async (req, res, next) => {
     if (process.env.node_env === 'development') {
       console.log(err.code);
     }
+
+    // Respond with 400 if the user enters blank object
+    if (err.message === "400")
+      return res.status(400).json({ status: 400, message: "Bad Request"});
 
     switch (err.code) {
       // Handle "auth/email-already-in-use" error appropriately and respond with 204
