fa2994f1f1aabf60fe36bb64ac8fccf757a673aa
davidtaing <adavidtaing@gmail.com>
2021-09-14 00:01:00 +1000

    test: refactor config into separate file

---
+++ users-api/tests/config.js
@@ -0,0 +1,10 @@
+const { projectId : FIREBASE_PROJECT_ID } = config.FIREBASE_CONFIGS;
+
+// DELETE URL: Removes all user accounts in emulator database.
+// NOTE: Only works for emulator and not production database.
+export const WIPE_USERS_URL = `http://localhost:9099/emulator/v1/projects/${FIREBASE_PROJECT_ID}/accounts`;
+
+export const DEFAULT_TEST_USER = {
+  email: "hello@fakewebsite.com",
+  password: "12345678",
+}
--- users-api/tests/register.controller.test.js
+++ users-api/tests/register.controller.test.js
@@ -5,19 +5,10 @@ import fetch from "cross-fetch";
 import config from "../src/config/config";
 import server from "../src/app";
 
+import { DEFAULT_TEST_USER, WIPE_USERS_URL } from "./config";
+
 chai.use(chaiHttp);
 const { expect } = chai;
-
-const DEFAULT_TEST_USER = {
-  email: "hello@fakewebsite.com",
-  password: "12345678",
-}
-
-const { projectId : FIREBASE_PROJECT_ID } = config.FIREBASE_CONFIGS;
-
-// DELETE URL: Removes all user accounts in emulator database.
-// NOTE: Only works for emulator and not production database.
-const WIPE_USERS_URL = `http://localhost:9099/emulator/v1/projects/${FIREBASE_PROJECT_ID}/accounts`
 
 /**
  * TODO: Test Flow
