789935269fdd83802fbfc168f8f4cad631d17676
davidtaing <adavidtaing@gmail.com>
2021-09-14 00:06:53 +1000

    docs: added comment overview of tests

--- users-api/tests/register.controller.test.js
+++ users-api/tests/register.controller.test.js
@@ -11,13 +11,13 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 /**
- * TODO: Test Flow
- * Source: https://firebase.google.com/docs/emulator-suite/connect_auth
- * 
- * 1. Create Users with Authentication signUp REST Endpoint
- * 2. Sign in users using the emails and passwords to perform tests.
- * 3. If applicable to your tests, fetch available out-of-band email verification codes from the emulator-specific REST endpont.
- * 4. Flush user records with the emulator-specific REST endpoint for clearing data.
+ * Before: Wipe users database
+ * Tests:
+ *     - Succesfully register user
+ *     - Reregister same user with same credentials as previous test
+ *     - Invalid email
+ *     - Empty user object
+ * Before: Wipe users database again
  */
 
 describe("User Registration", () => {
