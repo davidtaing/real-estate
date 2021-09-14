482221c8bc3c0e5499b659547d8d6830219f6214
davidtaing <adavidtaing@gmail.com>
2021-09-14 16:51:54 +1000

    test: cleanup flushFirebaseUsers util function

--- users-api/tests/test-utils.js
+++ users-api/tests/test-utils.js
@@ -3,14 +3,9 @@ import { WIPE_USERS_URL } from "./config";
 
 export const flushFirebaseUsers = async () => {
   try {
-    console.log("Reset test database: deleting all users.");
-
-    const result = await fetch(WIPE_USERS_URL, {
+    return await fetch(WIPE_USERS_URL, {
       method: "DELETE",
     });
-    console.log("Sucessfully reset test users database.");
-    
-    return result;
   } catch (err) {
     console.log("Failed to reset test users database.");
     console.error(err);
