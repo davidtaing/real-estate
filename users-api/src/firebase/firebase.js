eb94bfdd5fe13aba80afd3b634b2982a081a3cbe
davidtaing <adavidtaing@gmail.com>
2021-09-20 17:03:40 +1000

    Remove users-api In Preparation for Rewrite

--- users-api/src/firebase/firebase.js
+++
@@ -1,8 +0,0 @@
-import { initializeApp } from "firebase/app";
-import config from "../config/config";
-
-const { FIREBASE_CONFIGS } = config;
-
-const app = initializeApp(FIREBASE_CONFIGS);
-
-export default app;
