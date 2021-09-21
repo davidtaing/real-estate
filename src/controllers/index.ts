07f2fc4cb2686ab707d99666930e99fe23d40bae
davidtaing <adavidtaing@gmail.com>
2021-09-21 21:53:49 +1000

    Init Payment Controller from lb4 cli

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -2,3 +2,4 @@ export * from './ping.controller';
 export * from './property-tenant.controller';
 export * from './property-payment.controller';
 export * from './tenant-payment.controller';
+export * from './payment.controller';
