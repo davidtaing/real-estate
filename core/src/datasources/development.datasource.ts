f8108081b6c5dd409996d98e199866c01ec7e65e
davidtaing <adavidtaing@gmail.com>
2021-09-25 22:53:00 +1000

    Chore: Revert to Development Database

--- core/src/datasources/development.datasource.ts
+++ core/src/datasources/development.datasource.ts
@@ -9,7 +9,7 @@ const config = {
   port: 5432,
   user: 'postgres',
   password: 'dj3zlau0V?oQ#0S',
-  database: 'development-db-design-first'
+  database: 'development'
 };
 
 // Observe application's life cycle to disconnect the datasource when
